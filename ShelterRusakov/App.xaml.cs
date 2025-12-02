699d09039a6b6d6996ef7e266eee26f3e0a9835f
vboxuser <vboxuser@WIN10>
2025-12-02 11:37:02 +0300

    Добавьте файлы проекта.

---
+++ ShelterRusakov/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ShelterRusakov.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ShelterRusakov
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ShelterRusakovEntities context = new ShelterRusakovEntities();
+    }
+}
