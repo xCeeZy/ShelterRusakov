98d13abe15d7f7d5be127754d153e4c228dbedc5
vboxuser <rusakov23032006@gmail.com>
2025-12-02 12:27:26 +0300

    Добавил файлы проекта

--- ShelterRusakov/MainWindow.xaml.cs
+++ ShelterRusakov/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShelterRusakov.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,19 +16,58 @@ using System.Windows.Shapes;
 
 namespace ShelterRusakov
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            TypeComboBox.SelectedValuePath = "id";
+            TypeComboBox.DisplayMemberPath = "name";
+            TypeComboBox.ItemsSource = App.context.viewanimal.ToList();
+
+            AnimalsDataGrid.ItemsSource = App.context.journal.ToList();
         }
 
         private void OkButton_Click(object sender, RoutedEventArgs e)
         {
+            if (DateAdmissionDp.SelectedDate == null ||
+                TypeComboBox.SelectedItem == null ||
+                string.IsNullOrEmpty(NicknameTextBox.Text) ||
+                string.IsNullOrEmpty(AgeTextBox.Text) ||
+                string.IsNullOrEmpty(ConditionsTextBox.Text) ||
+                DateEndDp.SelectedDate == null)
+            {
+                MessageBox.Show("Заполните все поля");
+            }
+            else
+            {
+                journal NewJournal = new journal()
+                {
+                    datestart = DateAdmissionDp.SelectedDate.Value,
+                    idviewanimal = (int)TypeComboBox.SelectedValue,
+                    name = NicknameTextBox.Text,
+                    pasport = Convert.ToBoolean(PassportCheckBox.IsChecked),
+                    age = int.Parse(AgeTextBox.Text),
+                    service = ConditionsTextBox.Text,
+                    dateend = DateEndDp.SelectedDate.Value
+                };
 
+                App.context.journal.Add(NewJournal);
+                App.context.SaveChanges();
+
+                MessageBox.Show("Запись добавлена");
+
+                DateAdmissionDp.SelectedDate = null;
+                TypeComboBox.SelectedItem = null;
+                NicknameTextBox.Text = "";
+                PassportCheckBox.IsChecked = false;
+                AgeTextBox.Text = "";
+                ConditionsTextBox.Text = "";
+                DateEndDp.SelectedDate = null;
+
+                AnimalsDataGrid.ItemsSource = App.context.journal.ToList();
+            }
         }
     }
 }
